9966b18da0c36ca5509b120dfc7db32cb625605c
JAghazada <jamil.aghazada03@gmail.com>
2023-03-24 00:11:23 +0400

    session problem solved

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const app = express();
 const routers = require("./routes/index")
 const session = require("express-session")
 const connect = require("./db/connection/connect");
-const MongoStore=require("connect-mongo")(session)
+const MongoStore = require("connect-mongo");
 require("dotenv").config();
 const PORT =process.env.PORT || 3003
 connect()
-
-const store = MongoStore.create({
-    mongoUrl:"mongodb://127.0.0.1:27017/SessionExample"
+const store =  MongoStore.create({
+    mongoUrl:process.env.DB_URI
 })
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
